691d5f3c1f2b3de1dd67e6f4bfe98a08ea03a79d
Emilie - G <guthier.emilie@gmail.com>
2025-06-11 14:49:39 +0200

    Push After tests ok

---
+++ EvalCedric/EvalCedric/Migrations/20250611124635_UpdateMigrationAfterTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EvalCedric.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMigrationAfterTests : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ EvalCedricTest/ErrorTest.cs
@@ -0,0 +1,75 @@
+﻿// Projet : EvalCedric.Tests
+// Fichier : ErrorTests.cs
+
+using System.Diagnostics;
+using Bunit;
+using EvalCedric.Components.Pages; // Assurez-vous que le chemin vers votre page Error est correct
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using TestContext = Bunit.TestContext;
+
+namespace EvalCedric.Tests
+{
+    [TestClass]
+    public class ErrorTests : TestContext
+    {
+        [TestMethod]
+        public void Test_Error_Affiche_Les_Messages_Par_Defaut()
+        {
+            // ARRANGE & ACT
+          
+            var cut = RenderComponent<Error>();
+
+            // ASSERT
+         
+            Assert.IsNotNull(cut.Find("h1.text-danger"));
+            Assert.IsTrue(cut.Find("h2.text-danger").TextContent.Contains("An error occurred"));
+
+          
+            Assert.AreEqual(0, cut.FindAll("p > strong:contains('Request ID:')").Count);
+        }
+
+        [TestMethod]
+        public void Test_Error_Avec_Activity_Affiche_RequestId()
+        {
+            // ARRANGE
+           
+            using var activity = new Activity("TestActivity");
+            activity.Start();
+
+            // ACT
+            var cut = RenderComponent<Error>();
+
+            // ASSERT
+          
+            var requestIdElement = cut.Find("code");
+            Assert.IsNotNull(requestIdElement);
+            Assert.AreEqual(activity.Id, requestIdElement.TextContent);
+        }
+
+        [TestMethod]
+        public void Test_Error_Avec_HttpContext_Affiche_TraceIdentifier()
+        {
+            // ARRANGE
+       
+            var fakeHttpContext = new DefaultHttpContext();
+            var testTraceId = "trace-id-pour-le-test-123";
+            fakeHttpContext.TraceIdentifier = testTraceId;
+
+            // ACT
+          
+            var cut = RenderComponent<CascadingValue<HttpContext>>(parameters => parameters
+                .Add(p => p.Value, fakeHttpContext) 
+                .AddChildContent<Error>()          
+            );
+
+            // ASSERT
+           
+            var requestIdElement = cut.Find("code");
+            Assert.IsNotNull(requestIdElement);
+            Assert.AreEqual(testTraceId, requestIdElement.TextContent);
+        }
+    }
+}
